f17c32e94c0be1460c06bbc960a5e4f21d316cc6
Rezoot <rezacz2002@gmail.com>
2025-06-05 21:01:29 +0200

    coworker test

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include <iostream>
 
 int main() {
-std :: string imagePath = "cat.jpg";
+
+std :: string imagePath = "photos\cat.jpg";
+
 // std :: string imagePath = "milkyway.jpg";
 cv :: Mat image = cv :: imread(imagePath, cv :: IMREAD_COLOR);
 cv::Mat edges;
@@ -10,6 +12,6 @@ cv::Canny(image, edges, 50, 150); // You can tweak these thresholds
 cv::imshow("Original", image);
 cv::imshow("Edges", edges);
 cv::waitKey(0); // Wait for a key press
-cv :: waitKey(0);
+
 return 0;
 }
