12e1b6bdf230a48e65938eda3a097c85f2540e80
Rezoot <rezacz2002@gmail.com>
2025-06-11 19:49:02 +0200

    final version 1.0

--- DatasImages.h
+++ DatasImages.h
@@ -11,82 +11,85 @@ class Images;  ///< Forward declaration klasy Images
 
 /**
  * @class Datas
- * @brief Klasa odpowiedzialna za zarz¹dzanie plikiem INI oraz œcie¿kami wejœcia i wyjœcia.
+ * @brief Klasa odpowiedzialna za zarzadzanie plikiem INI oraz sciezkami wejsciowa i wyjsciowa.
  *
- * Umo¿liwia wczytanie œcie¿ek z pliku INI, przetwarzanie plików graficznych oraz zapis wyników.
+ * Umozliwia wczytanie sciezek z pliku INI, przetwarzanie plikow graficznych oraz zapis wynikow.
  */
 class Datas {
 public:
-    std::string fileINI; ///< Œcie¿ka do pliku INI
-    std::string inputDir;   ///< Klucz okreœlaj¹cy katalog wejœciowy
-    std::string outputDir;  ///< Klucz okreœlaj¹cy katalog wyjœciowy
+    std::string fileINI;    ///< Sciezka do pliku INI
+    std::string inputDir;   ///< Sciezka do katalogu wejsciowego
+    std::string outputDir;  ///< Sciezka do katalogu wyjsciowego
 
     /**
      * @brief Konstruktor klasy Datas.
      *
-     * Inicjalizuje dane z pliku INI i rozpoczyna przetwarzanie obrazów z rozszerzeniami .jpg i .png.
+     * Inicjalizuje dane z pliku INI i rozpoczyna przetwarzanie obrazow z rozszerzeniami .jpg i .png.
      *
-     * @param FileINI Œcie¿ka do pliku INI.
-     * @param Input Klucz w pliku INI okreœlaj¹cy folder wejœciowy.
-     * @param Output Klucz w pliku INI okreœlaj¹cy folder wyjœciowy.
+     * @param FileINI Sciezka do pliku INI.
+     * @param Input Klucz okreslajacy katalog wejsciowy.
+     * @param Output Klucz okreslajacy katalog wyjsciowy.
      */
     Datas(std::string FileINI, std::string Input, std::string Output);
 
     /**
      * @brief Zapisuje przetworzony obraz do katalogu wynikowego.
      *
-     * @param edges Obiekt obrazu (np. wynik detekcji krawêdzi).
+     * @param obraz Obiekt obrazu (np. wynik detekcji krawedzi).
      * @param name Nazwa pliku wynikowego.
      */
-    void save2path(const cv::Mat& edges, const std::string& name);
+    void save2path(const cv::Mat& obraz, const std::string& name);
 
 private:
     /**
-     * @brief Przetwarza obrazy w katalogu wejœciowym o podanym rozszerzeniu.
+     * @brief Przetwarza obrazy w katalogu wejsciowym o podanym rozszerzeniu.
      *
-     * Tworzy osobne w¹tki do przetwarzania ka¿dego obrazu.
+     * Tworzy osobne watki do przetwarzania kazdego obrazu.
      *
-     * @param extension Rozszerzenie plików (np. "jpg" lub "png").
-     * @param images Referencja do obiektu Images do przetwarzania obrazów.
-     * @param threads Wektor aktywnych w¹tków.
-     * @param count Licznik przetworzonych obrazów.
+     * @param extension Rozszerzenie plikow (np. "jpg" lub "png").
+     * @param images Referencja do obiektu Images odpowiedzialnego za przetwarzanie obrazow.
+     * @param threads Wektor aktywnych watkow.
+     * @param count Licznik przetworzonych obrazow.
      */
     void processFilesWithExtension(const std::string& extension, Images& images, std::vector<std::thread>& threads, int& count);
 
     /**
-     * @brief Odczytuje wartoœæ przypisan¹ do danego klucza z pliku INI.
+     * @brief Odczytuje wartosc przypisana do danego klucza z pliku INI.
      *
-     * @param key Klucz, którego wartoœæ ma zostaæ pobrana.
-     * @return std::string Zwraca wartoœæ przypisan¹ do klucza lub pusty string, jeœli nie znaleziono.
+     * @param key Klucz, ktorego wartosc ma zostac pobrana.
+     * @return std::string Wartosc przypisana do klucza lub pusty string, jesli nie znaleziono.
      */
     std::string getValue(const std::string& key);
 };
 
 /**
  * @class Images
- * @brief Klasa odpowiedzialna za przetwarzanie obrazów (np. detekcjê krawêdzi).
+ * @brief Klasa odpowiedzialna za przetwarzanie obrazow (np. detekcje krawedzi).
  *
- * Umo¿liwia asynchroniczne przetwarzanie obrazów oraz prezentacjê ich siatki.
+ * Umozliwia asynchroniczne przetwarzanie obrazow oraz prezentacje ich siatki.
  */
 class Images {
 private:
-    std::mutex coutMutex;              ///< Mutex do synchronizacji wypisywania na konsolê
-    std::mutex datasMutex;             ///< Mutex do synchronizacji zapisu danych
-    std::mutex imagesMutex;            ///< Mutex do synchronizacji dostêpu do obrazów
-    std::vector<cv::Mat> collectedImages; ///< Kolekcja przetworzonych obrazów
+    std::mutex coutMutex;               ///< Mutex do synchronizacji wypisywania na konsole
+    std::mutex datasMutex;              ///< Mutex do synchronizacji zapisu danych
+    std::mutex imagesMutex;             ///< Mutex do synchronizacji dostepu do obrazow
+    std::vector<cv::Mat> collectedImages; ///< Kolekcja przetworzonych obrazow
+    std::vector<cv::Mat> originalImages;  ///< Kolekcja oryginalnych obrazow
 
 public:
     /**
-     * @brief Wyœwietla siatkê przetworzonych obrazów w oknie OpenCV.
+     * @brief Wyswietla siatke przetworzonych i orginalnych obrazow w oknie OpenCV.
+     *
+     * @param datas Referencja do obiektu Datas, opcjonalnie uzywana przy prezentacji.
      */
-    void showImageGrid();
+    void showImageGrid(Datas& datas);
 
     /**
-     * @brief Przetwarza obraz, wykonuje detekcjê krawêdzi i zapisuje wynik.
+     * @brief Przetwarza obraz, wykonuje detekcje krawedzi i zapisuje wynik.
      *
-     * @param filePath Pe³na œcie¿ka do pliku obrazu.
+     * @param filePath Pelna sciezka do pliku obrazu.
      * @param filename Nazwa pliku.
-     * @param datas Referencja do obiektu Datas, który zarz¹dza zapisem obrazu.
+     * @param datas Referencja do obiektu Datas, ktory zarzadza zapisem obrazu.
      */
     void processImage(const std::string& filePath, const std::string& filename, Datas& datas);
 };
--- main.cpp
+++ main.cpp
@@ -5,22 +5,26 @@
 
 #include "DatasImages.h"
 
-/**
- * @brief G³ówna funkcja programu.
- * 
- * Inicjalizuje obiekty, przetwarza pliki .jpg i .png z katalogu wejœciowego i zapisuje je w katalogu wyjœciowym.
- */
+ /**
+  * @brief G³ówna funkcja programu.
+  *
+  * Inicjalizuje obiekty, przetwarza pliki .jpg i .png z katalogu wejœciowego i zapisuje je w katalogu wyjœciowym.
+  */
 int main() {
 
-
-
+    std::cout << "test1" << std::endl;
     Datas datas("config.ini", "source", "destination");//idealny scenariusz
-    
-    Datas datas("config.ini", "source", "testowy2");//brak folderu output
-    
-    Datas datas3("config.ini", "testowy", "testowy2");//brak wej i wyj
 
 
+    std::cout << std::endl << "test2" << std::endl;
+    Datas datas2("config.ini", "source", "testowy2");//brak folderu output
+
+
+    std::cout << std::endl << "test3" << std::endl;
+    Datas datas3("config.ini", "testowy", "destination");//brak folderu input
+
+
+    std::cout << std::endl << "test4" << std::endl;
     Datas datas4("CMakeLists.txt", "testowy", "testowy2");//inny plik wej
 
 
