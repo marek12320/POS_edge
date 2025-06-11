53819a05ee162f50c29cee9c2ccda89622358126
Rezoot <rezacz2002@gmail.com>
2025-06-11 19:42:37 +0200

    Revert "final version 1.0"
    This reverts commit 07f6e4c51cb85385085147612157ce87bb99141c.

--- Datas.cpp
+++ Datas.cpp
@@ -1,55 +1,19 @@
 #include "DatasImages.h"
 
 Datas::Datas(std::string FileINI, std::string Input, std::string Output) {
-    // Zapisanie sciezki do pliku INI
     fileINI = FileINI;
-
-    // Pobranie wartosci dla folderu wejsciowego i wyjsciowego z pliku INI
     inputDir = getValue(Input);
     outputDir = getValue(Output);
 
-    char odp;
 
-    // Sprawdzenie, czy obie sciezki sa poprawne
-    if (outputDir == "" || inputDir == "") {
-        std::cout << "nie podano sciezki wyjsciowej lub wejsciowej";
-        return;
-    }
 
-    // Sprawdzenie, czy folder wyjsciowy istnieje
-    DWORD ftyp = GetFileAttributesA(outputDir.c_str());
-    if (ftyp == INVALID_FILE_ATTRIBUTES || !(ftyp & FILE_ATTRIBUTE_DIRECTORY)) {
-        // Jesli folder nie istnieje, zapytaj uzytkownika o utworzenie
-        std::cout << "Folder wyjsciowy [" << outputDir << "] nie istnieje. " << std::endl << "Czy chcesz go utworzyc: t? ";
-        
-        std::cin >> odp;
-
-        if (odp == 't' || odp == 'T') {
-            // Proba utworzenia folderu
-            if (!CreateDirectoryA(outputDir.c_str(), NULL)) {
-                std::cerr << "Nie udalo sie utworzyc folderu: " << outputDir << std::endl;
-                return;
-            }
-            else {
-                std::cout << "Utworzono folder: " << outputDir << std::endl;
-            }
-        }
-        else {
-            std::cout << "Pomijanie operacji - folder wyjsciowy nie istnieje." << std::endl;
-            return;
-        }
-    }
-
-    // Tworzenie instancji obiektu Images i listy watkow
     Images images;
     std::vector<std::thread> threads;
     int count = 0;
 
-    // Przetwarzanie plikow z rozszerzeniem .jpg i .png
     processFilesWithExtension("jpg", images, threads, count);
     processFilesWithExtension("png", images, threads, count);
 
-    // Oczekiwanie na zakonczenie wszystkich watkow
     for (auto& t : threads) {
         if (t.joinable()) {
             t.join();
@@ -57,78 +21,56 @@ Datas::Datas(std::string FileINI, std::string Input, std::string Output) {
         }
     }
 
-    // Informacja o wyniku przetwarzania
     if (count == 0) {
-        std::cout << "bledna sciezka dostepu lub brak rozszerzen jpg/png [" << inputDir << "]" << std::endl << "dla pliku " << FileINI << std::endl;
+        std::cout << "bledna sciezka dostepu lub brak rozszerzen jpg/png dla pliku " << FileINI << std::endl;
     }
     else {
         std::cout << "przetworzono " << count << " obrazow dla pliku " << FileINI << std::endl;
-        std::cout << "czy stworzyc mozaiki, moze to potrwac troche dluzej: t? ";
-        std::cin >> odp;
-
-        if (odp == 't' || odp == 'T') {
-            // Wyswietlenie mozaiki z obrazow
-            images.showImageGrid(std::ref(*this));
-        }
-        
+        images.showImageGrid();
     }
+
+    
 }
 
-void Datas::save2path(const cv::Mat& obraz, const std::string& name) {
-    // Zbudowanie pelnej sciezki zapisu
+void Datas::save2path(const cv::Mat& edges, const std::string& name) {
     std::string savePath = outputDir + "\\" + name;
-    // Zapis obrazu do pliku
-    cv::imwrite(savePath, obraz);
+    cv::imwrite(savePath, edges);
 }
 
 std::string Datas::getValue(const std::string& key) {
-    // Otworzenie pliku INI
     std::ifstream file(fileINI);
     std::string line;
-
-    // Przegladanie pliku linia po linii
     while (std::getline(file, line)) {
         size_t pos = line.find('=');
         if (pos != std::string::npos) {
             std::string foundKey = line.substr(0, pos);
             std::string value = line.substr(pos + 1);
-            // Sprawdzenie, czy znaleziono zadany klucz
             if (foundKey == key) {
                 return value;
             }
         }
     }
-
-    // Zwraca pusty string, jesli nie znaleziono
     return "";
 }
 
 void Datas::processFilesWithExtension(const std::string& extension, Images& images, std::vector<std::thread>& threads, int& count) {
-    // Jesli folder wejsciowy pusty, nic nie rob
     if (inputDir.empty()) return;
 
-    // Tworzenie wzorca do wyszukiwania plikow z podanym rozszerzeniem
     std::string searchPath = inputDir + "\\*." + extension;
     WIN32_FIND_DATAA fileData;
     HANDLE hFind = FindFirstFileA(searchPath.c_str(), &fileData);
-
-    // Ustalenie maksymalnej liczby watkow na podstawie CPU
     const unsigned int MAX_THREADS = std::thread::hardware_concurrency();
 
-    // Jesli nie znaleziono zadnych plikow
     if (hFind == INVALID_HANDLE_VALUE)
         return;
 
     do {
-        // Pomijanie folderow
         if (fileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
             continue;
 
-        // Pobranie nazwy i pelnej sciezki do pliku
         std::string filename = fileData.cFileName;
         std::string filePath = inputDir + "\\" + filename;
 
-        // Ograniczenie liczby aktywnych watkow
         while (threads.size() >= MAX_THREADS) {
             for (auto it = threads.begin(); it != threads.end();) {
                 if (it->joinable()) {
@@ -143,11 +85,9 @@ void Datas::processFilesWithExtension(const std::string& extension, Images& imag
             std::this_thread::sleep_for(std::chrono::milliseconds(10));
         }
 
-        // Dodanie nowego watku do przetwarzania obrazu
         threads.emplace_back(&Images::processImage, &images, filePath, filename, std::ref(*this));
 
     } while (FindNextFileA(hFind, &fileData) != 0);
 
-    // Zwolnienie zasobow systemowych
     FindClose(hFind);
 }
--- DatasImages.h
+++ DatasImages.h
@@ -11,85 +11,82 @@ class Images;  ///< Forward declaration klasy Images
 
 /**
  * @class Datas
- * @brief Klasa odpowiedzialna za zarzadzanie plikiem INI oraz sciezkami wejsciowa i wyjsciowa.
+ * @brief Klasa odpowiedzialna za zarz¹dzanie plikiem INI oraz œcie¿kami wejœcia i wyjœcia.
  *
- * Umozliwia wczytanie sciezek z pliku INI, przetwarzanie plikow graficznych oraz zapis wynikow.
+ * Umo¿liwia wczytanie œcie¿ek z pliku INI, przetwarzanie plików graficznych oraz zapis wyników.
  */
 class Datas {
 public:
-    std::string fileINI;    ///< Sciezka do pliku INI
-    std::string inputDir;   ///< Sciezka do katalogu wejsciowego
-    std::string outputDir;  ///< Sciezka do katalogu wyjsciowego
+    std::string fileINI; ///< Œcie¿ka do pliku INI
+    std::string inputDir;   ///< Klucz okreœlaj¹cy katalog wejœciowy
+    std::string outputDir;  ///< Klucz okreœlaj¹cy katalog wyjœciowy
 
     /**
      * @brief Konstruktor klasy Datas.
      *
-     * Inicjalizuje dane z pliku INI i rozpoczyna przetwarzanie obrazow z rozszerzeniami .jpg i .png.
+     * Inicjalizuje dane z pliku INI i rozpoczyna przetwarzanie obrazów z rozszerzeniami .jpg i .png.
      *
-     * @param FileINI Sciezka do pliku INI.
-     * @param Input Klucz okreslajacy katalog wejsciowy.
-     * @param Output Klucz okreslajacy katalog wyjsciowy.
+     * @param FileINI Œcie¿ka do pliku INI.
+     * @param Input Klucz w pliku INI okreœlaj¹cy folder wejœciowy.
+     * @param Output Klucz w pliku INI okreœlaj¹cy folder wyjœciowy.
      */
     Datas(std::string FileINI, std::string Input, std::string Output);
 
     /**
      * @brief Zapisuje przetworzony obraz do katalogu wynikowego.
      *
-     * @param obraz Obiekt obrazu (np. wynik detekcji krawedzi).
+     * @param edges Obiekt obrazu (np. wynik detekcji krawêdzi).
      * @param name Nazwa pliku wynikowego.
      */
-    void save2path(const cv::Mat& obraz, const std::string& name);
+    void save2path(const cv::Mat& edges, const std::string& name);
 
 private:
     /**
-     * @brief Przetwarza obrazy w katalogu wejsciowym o podanym rozszerzeniu.
+     * @brief Przetwarza obrazy w katalogu wejœciowym o podanym rozszerzeniu.
      *
-     * Tworzy osobne watki do przetwarzania kazdego obrazu.
+     * Tworzy osobne w¹tki do przetwarzania ka¿dego obrazu.
      *
-     * @param extension Rozszerzenie plikow (np. "jpg" lub "png").
-     * @param images Referencja do obiektu Images odpowiedzialnego za przetwarzanie obrazow.
-     * @param threads Wektor aktywnych watkow.
-     * @param count Licznik przetworzonych obrazow.
+     * @param extension Rozszerzenie plików (np. "jpg" lub "png").
+     * @param images Referencja do obiektu Images do przetwarzania obrazów.
+     * @param threads Wektor aktywnych w¹tków.
+     * @param count Licznik przetworzonych obrazów.
      */
     void processFilesWithExtension(const std::string& extension, Images& images, std::vector<std::thread>& threads, int& count);
 
     /**
-     * @brief Odczytuje wartosc przypisana do danego klucza z pliku INI.
+     * @brief Odczytuje wartoœæ przypisan¹ do danego klucza z pliku INI.
      *
-     * @param key Klucz, ktorego wartosc ma zostac pobrana.
-     * @return std::string Wartosc przypisana do klucza lub pusty string, jesli nie znaleziono.
+     * @param key Klucz, którego wartoœæ ma zostaæ pobrana.
+     * @return std::string Zwraca wartoœæ przypisan¹ do klucza lub pusty string, jeœli nie znaleziono.
      */
     std::string getValue(const std::string& key);
 };
 
 /**
  * @class Images
- * @brief Klasa odpowiedzialna za przetwarzanie obrazow (np. detekcje krawedzi).
+ * @brief Klasa odpowiedzialna za przetwarzanie obrazów (np. detekcjê krawêdzi).
  *
- * Umozliwia asynchroniczne przetwarzanie obrazow oraz prezentacje ich siatki.
+ * Umo¿liwia asynchroniczne przetwarzanie obrazów oraz prezentacjê ich siatki.
  */
 class Images {
 private:
-    std::mutex coutMutex;               ///< Mutex do synchronizacji wypisywania na konsole
-    std::mutex datasMutex;              ///< Mutex do synchronizacji zapisu danych
-    std::mutex imagesMutex;             ///< Mutex do synchronizacji dostepu do obrazow
-    std::vector<cv::Mat> collectedImages; ///< Kolekcja przetworzonych obrazow
-    std::vector<cv::Mat> originalImages;  ///< Kolekcja oryginalnych obrazow
+    std::mutex coutMutex;              ///< Mutex do synchronizacji wypisywania na konsolê
+    std::mutex datasMutex;             ///< Mutex do synchronizacji zapisu danych
+    std::mutex imagesMutex;            ///< Mutex do synchronizacji dostêpu do obrazów
+    std::vector<cv::Mat> collectedImages; ///< Kolekcja przetworzonych obrazów
 
 public:
     /**
-     * @brief Wyswietla siatke przetworzonych i orginalnych obrazow w oknie OpenCV.
-     *
-     * @param datas Referencja do obiektu Datas, opcjonalnie uzywana przy prezentacji.
+     * @brief Wyœwietla siatkê przetworzonych obrazów w oknie OpenCV.
      */
-    void showImageGrid(Datas& datas);
+    void showImageGrid();
 
     /**
-     * @brief Przetwarza obraz, wykonuje detekcje krawedzi i zapisuje wynik.
+     * @brief Przetwarza obraz, wykonuje detekcjê krawêdzi i zapisuje wynik.
      *
-     * @param filePath Pelna sciezka do pliku obrazu.
+     * @param filePath Pe³na œcie¿ka do pliku obrazu.
      * @param filename Nazwa pliku.
-     * @param datas Referencja do obiektu Datas, ktory zarzadza zapisem obrazu.
+     * @param datas Referencja do obiektu Datas, który zarz¹dza zapisem obrazu.
      */
     void processImage(const std::string& filePath, const std::string& filename, Datas& datas);
 };
